201fdd80bb760e821f2fc618c1e87611baaf41fc
Maksym Zhmura <zmuramaksim@gmail.com>
2026-03-08 23:44:59 +0200

    Solution3

--- app/main.py
+++ app/main.py
@@ -1,8 +1,3 @@
-from app.baseRobot import BaseRobot
-from app.flyingRobot import FlyingRobot
-from app.deliveryDrone import DeliveryDrone # або як називається файл з дроном
-
-
 class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
